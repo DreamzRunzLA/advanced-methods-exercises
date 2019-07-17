def372b81e4b7f51068fbbbe979090c861533557
Salmaan (Sal) Qadir <37133322+DreamzRunzLA@users.noreply.github.com>
2019-07-17 00:33:42 -0600

    Add files via upload
    Did a few

--- advanced_methods_exercise/01_coprime.rb
+++ advanced_methods_exercise/01_coprime.rb
@@ -3,8 +3,26 @@
 # The method should return false otherwise. For example coprime?(25, 12) is true because
 # 1 is the only number that divides both 25 and 12.
 
+def coprime?(num_1, num_2)
+    #need to loop through all numbers of biggest num
+    #figure establish if there are no divisors
+    #return false as soon as you see one
+    #return true at the very end
+    arrParameters = [num_1, num_2]
+    greatestNum = arrParameters.max
+    allNums = *(2..greatestNum-1)
+    allNums.each do |ele|
+        if num_1 % ele == 0 && num_2 % ele == 0
+            return false
+        else
+            next
+        end
+    end
+    return true
+end
 
 p coprime?(25, 12)    # => true
 p coprime?(7, 11)     # => true
 p coprime?(30, 9)     # => false
 p coprime?(6, 24)     # => false
+p coprime?(144, 73)     # => false
--- advanced_methods_exercise/03_only_vowels.rb
+++ advanced_methods_exercise/03_only_vowels.rb
@@ -2,10 +2,23 @@
 # The method should return true if the string contains only vowels.
 # The method should return false otherwise.
 
+def only_vowels?(str)
+    flag = true
+    arrString = str.split('')
+    arrString.all? do |char|
+        if ['a', 'e', 'i', 'o', 'u'].include?(char) == false
+            flag = false
+        else
+            next
+        end
+    end
+    return flag
+end
 
-p only_vowels?("aaoeee")  # => true
-p only_vowels?("iou")     # => true
-p only_vowels?("cat")     # => false
-p only_vowels?("over")    # => false
-
-
+# p only_vowels?("aaoeee")  # => true
+# p only_vowels?("iou")     # => true
+# p only_vowels?("cat")     # => false
+# p only_vowels?("over")    # => false
+# p only_vowels?("ghthghtghthgyuo")    # => false
+# p only_vowels?("ghthghtghthg")    # => false
+# p only_vowels?("uuuuuuuuuaei")    # => true
